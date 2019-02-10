ff394e82b8e69b05240ce67571fd0b4230b6af2a
Christian Henz <chrhenz@gmx.de>
2019-02-10 23:48:08 +0100

    Makefile-based build for blackice2, using nextpnr instead of arachne

--- blackice2/clean.sh
+++ blackice2/clean.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
 
-PROG=atom
-
-# Remove BlackIce files
-rm -f $PROG.blif $PROG.txt $PROG.bin $PROG.log
-
-# Remove Simulation files
-rm -f a.out dump.vcd
+make clean
---
+++ blackice2/pre_pack.py
@@ -0,0 +1,1 @@
+ctx.addClock("clk100", 100)
